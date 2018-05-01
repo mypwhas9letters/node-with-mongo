49e7e2b939e063c62dc14053ef39a0eaa7c80cd5
Hui Wang <huiw3939@gmail.com>
2018-04-30 20:42:26 -0400

    another error

--- server/server.js
+++ server/server.js
@@ -3,8 +3,8 @@ var bodyParser = require('body-parser')
 var {ObjectId} = require('mongodb')
 
 var {mongoose} = require('./db/mongoose')
-var {Todo} = require('./models/Todo')
-var {User} = require('./models/User')
+var {Todo} = require('./models/todo')
+var {User} = require('./models/user')
 
 var app = express()
 const port = process.env.PORT || 3000
